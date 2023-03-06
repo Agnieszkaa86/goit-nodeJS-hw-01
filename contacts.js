0bc2137c5b476fa901b732f95c7cfe29c9955117
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-03-06 15:06:58 +0100

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises;
 const path = require("path");
-// const { nanoid } = require("nanoid");
+//const { nanoid } = require("nanoid");
 
 const contactsPath = path.resolve("./db/contacts.json");
 
@@ -12,7 +12,17 @@ const listContacts = async () => {
     console.error("Load error", error.message);
   }
 };
+const getContactById = async (contactId) => {
+  try {
+    const readContacts = await fs.readFile(contactsPath);
+    const jsonContacts = await JSON.parse(readContacts);
+    return jsonContacts.filter((cont) => cont.id === contactId);
+  } catch (error) {
+    console.error("Get contact error", error.message);
+  }
+};
 module.exports = {
-    listContacts
+  listContacts,
+  getContactById
 };
     
