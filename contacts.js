32314ac2241bcefdb3deb79980a01268f54b92f0
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-03-06 15:20:30 +0100

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises;
 const path = require("path");
-//const { nanoid } = require("nanoid");
+const { v4 } = require("uuid");
 
 const contactsPath = path.resolve("./db/contacts.json");
 
@@ -21,8 +21,39 @@ const getContactById = async (contactId) => {
     console.error("Get contact error", error.message);
   }
 };
+const removeContact = async (contactId) => {
+  try {
+    const readContacts = await fs.readFile(contactsPath);    
+    const jsonContacts = await JSON.parse(readContacts);
+    const contact = jsonContacts.filter((cont) => cont.id === contactId);
+    const removedContact = await jsonContacts.filter((cont) => cont.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(removedContact));
+    return contact;
+  } catch (error) {
+    console.error("Remove contact error", error.message);
+  }
+};
+const addContact = async(name, email, phone) => {
+  try {
+    const readContacts = await fs.readFile(contactsPath);
+    const jsonContacts = await JSON.parse(readContacts);
+    const addNewContact = {
+      id: v4(),
+      name,
+      email,
+      phone
+    }
+    const newContacts = [...jsonContacts, addNewContact];
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    return addNewContact;
+  } catch (error) {
+    console.error("Add contact error", error.message);
+  }
+}
 module.exports = {
   listContacts,
-  getContactById
+  getContactById,
+  removeContact,
+  addContact
 };
     
